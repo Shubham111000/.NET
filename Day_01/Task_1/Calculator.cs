ee1a96a79bfe8a66168dc10249240e6e4224933a
Shubham Gaikwad <shubham.gaikwad1011@gmail.com>
2023-10-23 20:22:54 +0530

    .net

---
+++ Day_01/Task_1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int product(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
