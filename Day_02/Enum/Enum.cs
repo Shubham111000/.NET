b3ee423a9ac7965aa82d91532a4ca6f9d0f398d8
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-10-24 15:41:12 +0530

    Enum

---
+++ Day_02/Enum/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    enum dep
+    {
+        MKT = 1,
+        ADV = 2,
+        ADMN = 3,
+    }
+}
