204677227c45c27dff01202cdb584bca756e8913
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-10-24 16:33:10 +0530

    Add files via upload

---
+++ Day_02/Task_3/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class Calculator
+    {
+        public float square(float x) {
+            return x * x;
+        }
+        public float cube (float x)
+        {
+            return x * x * x;
+        }
+        public int RoundOff(float x)
+        {
+            return ((int)x);
+        }    
+    }
+
+}
