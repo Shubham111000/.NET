eebb43f9ad0c278019abad5dff96feabed6789c5
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-10-25 19:36:32 +0530

    Add files via upload

---
+++ Day_04/Array TasK/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_TasK
+{
+    public class Account
+    {
+        static Account()
+        {
+            Console.WriteLine("Welcome to Bank of Baroda");
+        }
+        public static int Id;
+        string name; int id, balance;
+        public Account(string name, int balance)
+        {
+            this.id = ++Id;
+            this.name = name;
+            this.balance = balance;
+        }
+        public void deposit(int amt)
+        {
+            balance += amt;
+
+        }
+        public void withdraw(int amt)
+        {
+            if (amt <= balance)
+                balance -= amt;
+            else
+                Console.WriteLine("insufficient Balance");
+        }
+        public void Disp()
+        {
+            Console.WriteLine($" Id = {id} name = {name} balance = {balance}");
+        }
+    }
+}
