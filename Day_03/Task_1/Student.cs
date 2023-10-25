28823a7d61b4b7d5b5b3d785e47bca371521816f
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-10-25 19:14:17 +0530

    Day_03

---
+++ Day_03/Task_1/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Student
+    {
+        string name;
+        int maths;
+        int eng;
+        int sci;
+        int total;
+        public Student(string name, int maths, int eng, int sci)
+        {
+            this.name = name;
+            this.maths = maths;
+            this.eng = eng;
+            this.sci = sci;
+
+        }
+        public int TotalMarks()
+        {
+            this.total = maths + eng + sci;
+            return total;
+        }
+
+        public void disp()
+        {
+            Console.WriteLine($"name = {name} total = {TotalMarks()} ");
+        }
+    }
+}
