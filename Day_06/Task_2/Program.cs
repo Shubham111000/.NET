d1727d6c623dd29df25bec3a568ad7b16b2674d9
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-10-30 14:43:49 +0530

    Day_07

---
+++ Day_06/Task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+2) Write a method which will accept two number and return all even number between it as array and 
+   also give count in out variable.
+   int count;
+   int[] result = Dojob(5,30);
+*/
+
+namespace Task_2
+{
+    internal class Program
+    {
+        public static int[] Dojob(int x, int y)
+        {
+            int[] r = new int[((y - x) / 2) + 1];
+            if (x % 2 == 0)
+            {
+                for (int i = 0; i < r.Length; i++)
+                {
+                    r[i] = x;
+                    x += 2;
+                }
+            }
+            else
+            {
+                x = x + 1;
+                for (int i = 0; i < r.Length; i++)
+                {
+                    r[i] = x;
+                    x += 2;
+                }
+
+            }
+            return r;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] r = Dojob(4, 31);
+            foreach (int i in r)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
---
+++ Day_06/Task_3/CurrentAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class CurrentAccount:Account
+    {
+        public void Withdraw(Double Ammount) 
+        {
+            Balance -= Ammount;
+            Console.WriteLine($"Ammount of {Ammount} Is WIthdrwan");
+        }
+        public override string ToString()
+        {
+            return ($"Account ID is ={ID} Account Name is {Name} Accounts balance is {Balance} Account Type is Current Account");
+        }
+    }
+}
