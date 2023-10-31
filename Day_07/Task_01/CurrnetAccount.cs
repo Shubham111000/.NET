d705ee0ea41c2add7341c211f4f1d7b37164cbce
Shubham Gaikwad <74091710+Shubham111000@users.noreply.github.com>
2023-10-31 12:10:55 +0530

    Day_07

---
+++ Day_07/Task_01/CurrnetAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    internal class CurrentAccount : Account
+    {
+        public override void Withdraw(Double Ammount)
+        {
+            Balance -= Ammount;
+            Console.WriteLine($"Ammount of {Ammount} Is WIthdrwan");
+        }
+        public override string ToString()
+        {
+            return ($"Account ID is ={ID} Account Name is {Name} Accounts balance is {Balance} Account Type is Current Account");
+        }
+    }
+}
---
+++ Day_07/Task_01/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+//Create class Account having member id, name, balance.
+//Id autoincrement write read only property
+//Write getter setter for name and balance.
+//Balance can be modified only by child class.
+//This has virtual withdraw method which will 0.
+//It has deposit method.
+//Create two child class current and saving and override withdraw method. 
+//Create reference of Account class and point to saving account
+//and  current account. Using parent’s reference call withdraw method and deposit method.
+//Use ToString method to display status of the object [id name balance]
+*/
+
+using Task_3;
+
+namespace Task_01
+{
+    internal class Program
+    {
+        public static void WD(Account account) 
+        {
+            Console.WriteLine(account);
+            Console.WriteLine("Enter The Ammount you want to Withdraw");
+            account.Withdraw(int.Parse(Console.ReadLine()));
+        }
+        static void Main(string[] args)
+        {
+            Account account1 = new SavingsAccount() { Name = "Shubham", Balance = 45000 };
+            Account account2 = new CurrentAccount() { Name = "Sushil",  Balance = 50000 };
+            WD(account1);
+            WD(account2);
+            Console.WriteLine(account1);
+            Console.WriteLine(account2);
+
+
+
+        }
+    }
+}
