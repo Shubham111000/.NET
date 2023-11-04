24b05de7b6e0dbfaa4b2c8646c5dc5a9d72587e9
Shubham Gaikwad <shubham.gaikwad1011@gmail.com>
2023-11-04 13:05:51 +0530

    Commit

---
+++ Day_13/CURD application/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURD_application
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Id { get; set; }
+        public String MobileNumber { get; set; }
+    }
+}
