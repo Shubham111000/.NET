ed0ce37de23a96af68f8053e16252b080048f1cd
Shubham Gaikwad <shubham.gaikwad1011@gmail.com>
2023-11-04 17:29:40 +0530

    Insert

--- Day_13/CURD application/AdoDotNet.cs
+++ Day_13/CURD application/AdoDotNet.cs
@@ -1,12 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CURD_application
 {
     internal class AdoDotNet
     {
-    }
-}
+        private String _connetionstring;
+        public AdoDotNet(IConfiguration connetionstrings) 
+        {
+            _connetionstring=connetionstrings.GetConnectionString("Default");
+        }
+        public SqlConnection GetConnetion() 
+        {
+            SqlConnection sql = new SqlConnection(_connetionstring);
+                return sql;
+            
+        }
+         
+        public void Insert_Products(Customer e) 
+        {
+            
+            try
+            {
+                SqlConnection sql = GetConnetion();
+                SqlCommand cmd = new SqlCommand("@Insert", sql);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                
+                cmd.Parameters.Add("@pname", SqlDbType.NVarChar).Value = e.Name;
+                cmd.Parameters.AddWithValue("@paddress", SqlDbType.NVarChar).Value = e.Address;
+                cmd.Parameters.Add("@pmobileNumber", SqlDbType.NVarChar).Value = e.MobileNumber;
+
+                sql.Open();
+                Console.WriteLine("Connected");
+                int record = cmd.ExecuteNonQuery();
+                Console.WriteLine("Rows  Affected = " + record);
+            }
+            catch (SqlException se)
+            { Console.WriteLine(se.Message); }
+
+        }
+        public void Products() 
+        {
+            try
+            {
+                SqlConnection sql = GetConnetion();
+                SqlCommand cmd = new SqlCommand("Select * From Customer", sql); sql.Open();
+
+                Console.WriteLine("connected");
+                SqlDataReader rdr = cmd.ExecuteReader(); //returns object of sqldatareder
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", rdr["Id"], rdr["Name"], rdr["Address"], rdr["MobileNumber"]);
+                    }
+                }
+            }
+
+
+
+            catch (SqlException se)
+            { Console.WriteLine(se.Message); }
+        }
+
+
+                            }
+            
+
+        }
+
+  
--- Day_13/CURD application/Program.cs
+++ Day_13/CURD application/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+//using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CURD_application
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration _iconfigurations;
+        static void Main()
         {
+            GetAppsettings();
+            Insert_Products();
+            Print_products();
+        }
+        public static void GetAppsettings() 
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+           _iconfigurations = builder.Build();                   
+        }
+        public static void Insert_Products() 
+        {
+            AdoDotNet ado = new AdoDotNet(_iconfigurations);
+            ado.Insert_Products(new Customer() { Id=12,Name="MacBook",Address="Firozpur",MobileNumber="9205737517"});
+        }
+        public static void Print_products() 
+        {
+            AdoDotNet ado = new AdoDotNet(_iconfigurations);
+            ado.Products();
+
         }
     }
 }
